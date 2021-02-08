dd91988ecafb9f1ac15e28a46f276d11d7af3ac4
cristina <cristina@42network.org>
2021-02-08 17:37:33 +0100

    feat(describe): start display

---
+++ classes/feature.py
@@ -0,0 +1,51 @@
+class Feature:
+    def __init__(self, name, dataset):
+        self.name = name
+        self.count = len(dataset)
+        self.mean = sum(dataset) / self.count
+        self.std = self.__calc_std(dataset)
+        self.min = self.__calc_quartiles(dataset, 0)
+        self.q_25 = self.__calc_quartiles(dataset, 25)
+        self.q_50 = self.__calc_quartiles(dataset, 50)
+        self.q_75 = self.__calc_quartiles(dataset, 75)
+        self.max = self.__calc_quartiles(dataset, 100)
+    
+    def __calc_std(self, dataset):
+        sum_squares = 0
+        for i in range(len(dataset)):
+            sum_squares += (dataset[i] - self.mean) ** 2
+        std = sum_squares / (self.count - 1)
+        std = std ** 0.5
+        return std
+    
+    def __calc_quartiles(self, dataset, quartile):
+        dataset.sort()
+        position_min = (float(quartile) / 100) * (self.count - 1)
+        position_max_coef = position_min - int(position_min)
+        if position_max_coef == 0.0:
+            return dataset[int(position_min)]
+        position_max = position_min + 1
+        position_min_coef = 1 - position_max_coef
+        result_min = (dataset[int(position_min)] * position_min_coef)
+        result_max = (dataset[int(position_max)] * position_max_coef)
+        return result_min + result_max
+    
+    def get(self, info):
+        if info == 'name':
+            return str(self.name)
+        if info == 'count':
+            return str('{:.6f}'.format(self.count))
+        if info == 'mean':
+            return str('{:.6f}'.format(self.mean))
+        if info == 'std':
+            return str('{:.6f}'.format(self.std))
+        if info == 'min':
+            return str('{:.6f}'.format(self.min))
+        if info == 'q_25':
+            return str('{:.6f}'.format(self.q_25))
+        if info == 'q_50':
+            return str('{:.6f}'.format(self.q_50))
+        if info == 'q_75':
+            return str('{:.6f}'.format(self.q_75))
+        if info == 'max':
+            return str('{:.6f}'.format(self.max))
--- describe.py
+++ describe.py
@@ -3,6 +3,7 @@ try:
     import argparse
     import pandas as pd
     import numpy as np
+    from classes.feature import Feature
 except:
     print('[Import error] Please run <pip install -r requirements.txt>')
     exit()
@@ -50,68 +51,38 @@ def read_csv(datafile):
     except:
         raise NameError('[Read error] Wrong file format. Make sure you give an existing .csv file as argument.')
 
-def filter_dataset(X):
+def remove_empty_values(X):
     x_filtered = []
     for x in X:
         if x == x:
             x_filtered.append(x)
     return x_filtered
 
-def calc_count(X):
-    return len(X)
-
-def calc_mean(X, count):
-    return sum(X) / count
-
-def calc_std(X, mean, count):
-    sum_squares = 0
-    for i in range(len(X)):
-        sum_squares += (X[i] - mean) ** 2
-    std = sum_squares / (count - 1)
-    std = std ** 0.5
-    return std
-
-def calc_quartiles(X, quartile, count):
-    X.sort()
-    position_min = (float(quartile) / 100) * (count - 1)
-    position_max_coef = position_min - int(position_min)
-    if position_max_coef == 0.0:
-        return X[int(position_min)]
-    position_max = position_min + 1
-    position_min_coef = 1 - position_max_coef
-    result_min = (X[int(position_min)] * position_min_coef)
-    result_max = (X[int(position_max)] * position_max_coef)
-    return result_min + result_max
-
-def describe(features, X):
+def describe(features_names, X):
     i = 0
+    features = []
     for x in X:
-        print(features[i])
-        x_filtered = filter_dataset(x)
-        count = calc_count(x_filtered)
-        mean = calc_mean(x_filtered, count)
-        std = calc_std(x_filtered, mean, count)
-        min = calc_quartiles(x_filtered, 0, count)
-        quartile_25 = calc_quartiles(x_filtered, 25, count)
-        quartile_50 = calc_quartiles(x_filtered, 50, count)
-        quartile_75 = calc_quartiles(x_filtered, 75, count)
-        max = calc_quartiles(x_filtered, 100, count)
-        print("count : {:.6f}".format(count))
-        print("mean  : {:.6f}".format(mean))
-        print("std   : {:.6f}".format(std))
-        print("min   : {:.6f}".format(min))
-        print("25%   : {:.6f}".format(quartile_25))
-        print("50%   : {:.6f}".format(quartile_50))
-        print("75%   : {:.6f}".format(quartile_75))
-        print("max   : {:.6f}\n".format(max))
+        feature = Feature(features_names[i], remove_empty_values(x))
+        features.append(feature)
         i +=1
+    return features
+
+def display(features):
+    i = 0
+    information = ['name', 'count', 'mean', 'std', 'min', 'q_25', 'q_50', 'q_75', 'max']
+    for info in information:
+        data = []
+        for feature in features:
+            data.append(feature.get(info))
+        print('{:15s} {:15s} {:15s} {:15s} {:15s} {:15s} {:15s}'.format(information[i], data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7]))
+        i += 1
 
 def main():
     try:
         args = parse_arg()
-        X, y, features = read_csv(args.datafile)
-        # print(features)
-        describe(features, X)
+        X, y, features_names = read_csv(args.datafile)
+        features = describe(features_names, X)
+        display(features)
 
         # Real describe function as reference
         f = pd.read_csv(args.datafile)
