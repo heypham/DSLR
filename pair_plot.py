3d322c29a65ca2fc93c3d6eb8cd5bc45a18b31e1
Cristina Fernandez Bornay <cristinafernandezbornay@outlook.com>
2021-02-16 16:43:10 +0100

    feat(pair_plot): working for real

--- pair_plot.py
+++ pair_plot.py
@@ -70,24 +70,45 @@ def clasify_data_per_house(data, house):
     except:
         return [[], []]
 
+def get_title(feature_name):
+    if feature_name == 'Defense Against the Dark Arts':
+        return 'Defense'
+    if feature_name == 'Care of Magical Creatures':
+        return 'Care MC.'
+    if feature_name == 'History of Magic':
+        return 'Magic H.'
+    if feature_name == 'Muggle Studies':
+        return 'Muggle S.'
+    return feature_name
+
+def format_axis(axs, feature_1, feature_2):
+    axs[feature_1, feature_2].set_xticklabels([])
+    axs[feature_1, feature_2].set_yticklabels([])
+    axs[feature_2, feature_1].set_xticklabels([])
+    axs[feature_2, feature_1].set_yticklabels([])
+    return axs
+
 def main():
     try:
         args = parse_arg()
         X, y, features_names = read_csv(args.datafile)
         nb_features = 13
+        houses = ['Gryffindor', 'Slytherin', 'Hufflepuff', 'Ravenclaw']
         fig, axs = plt.subplots(nb_features, nb_features)
         for feature_1 in range(nb_features):
+            axs[0, feature_1].set_title(get_title(features_names[feature_1]))
+            plt.setp(axs[feature_1, 0], ylabel='{}'.format(get_title(features_names[feature_1])))
             for feature_2 in range(feature_1, nb_features):
+                axs = format_axis(axs, feature_1, feature_2)
                 data = filter_data(X[feature_1], X[feature_2], y[0])
-                houses = ['Gryffindor', 'Slytherin', 'Hufflepuff', 'Ravenclaw']
                 for house in houses:
                     [X1_per_house, X2_per_house] = clasify_data_per_house(data, house)
                     if feature_1 == feature_2:
-                        axs[feature_1, feature_2].hist(X1_per_house, alpha=0.2)
+                        axs[feature_1, feature_1].hist(X1_per_house, alpha=0.6)
                     else:
-                        axs[feature_1, feature_2].scatter(X1_per_house, X2_per_house, alpha=0.2, marker='.')
-                        axs[feature_2, feature_1].scatter(X1_per_house, X2_per_house, alpha=0.2, marker='.')
-        # plt.figsize(6.4, 4.8)
+                        axs[feature_1, feature_2].scatter(X2_per_house, X1_per_house, alpha=0.07, marker='.')
+                        axs[feature_2, feature_1].scatter(X1_per_house, X2_per_house, alpha=0.07, marker='.')
+        plt.suptitle('Pair plot')
         plt.show()
     except NameError as e:
         print(e)
