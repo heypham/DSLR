e469d5ffbd4d82b24b3afe4a8265eb0970f50f02
cristina <cristina@42network.org>
2021-03-05 16:40:31 +0100

    feat(log reg): adding try except eveywhere

--- classes/logisticregression.py
+++ classes/logisticregression.py
@@ -27,10 +27,13 @@ class LogisticRegression(object):
         self.thetas = np.zeros((14, 4))
 
     def parse_arg(self):
-        parser = argparse.ArgumentParser(prog='describe', usage='%(prog)s [-h] datafile.csv', description='Program describing the dataset given.')
-        parser.add_argument('datafile', help='the .csv file containing the dataset')
-        args = parser.parse_args()
-        return args
+        try:
+            parser = argparse.ArgumentParser(prog='describe', usage='%(prog)s [-h] datafile.csv', description='Program describing the dataset given.')
+            parser.add_argument('datafile', help='the .csv file containing the dataset')
+            args = parser.parse_args()
+            return args
+        except:
+            raise NameError('[Parse error] There has been an error while parsing the arguments.')
     
     def read_csv(self, datafile):
         """
@@ -40,12 +43,9 @@ class LogisticRegression(object):
         try:
             f = pd.read_csv(datafile)
             houses = f['Hogwarts House'].unique()
-            # print(houses)
             features = list(f.columns[6:])
             y = f['Hogwarts House']
             X = f.drop(['Index','Hogwarts House', 'First Name', 'Last Name', 'Birthday', 'Best Hand'],axis=1)
-            
-            # Transform arrays as numpy arrays for calculations
             self.X = np.array(X).T
             self.y = np.array([y])
             self.features = np.array(features).T
@@ -54,62 +54,77 @@ class LogisticRegression(object):
             raise NameError('[Read error] Wrong file format. Make sure you give an existing .csv file as argument.')
 
     def clean_data(self, X, y):
-        X_clean = []
-        y_clean = []
-        for i in range(X.shape[0]):
-            empty_value = 0
-            for j in range(X.shape[1]):
-                if X[i][j] != X[i][j]:
-                    empty_value = 1
-            if empty_value == 0:
-                X_clean.append(X[i])
-                y_clean.append(y[0][i])
-        X_clean = np.array(X_clean)
-        y_clean = np.array(y_clean).reshape(-1,1)
-        return X_clean.T, y_clean
+        try:
+            X_clean = []
+            y_clean = []
+            for i in range(X.shape[0]):
+                empty_value = 0
+                for j in range(X.shape[1]):
+                    if X[i][j] != X[i][j]:
+                        empty_value = 1
+                if empty_value == 0:
+                    X_clean.append(X[i])
+                    y_clean.append(y[0][i])
+            X_clean = np.array(X_clean)
+            y_clean = np.array(y_clean).reshape(-1,1)
+            return X_clean.T, y_clean
+        except:
+            raise NameError('[Process error] There has been an error while processing.')
 
     def split_data(self, X, y):
         """
         Splitting dataset into training data/testing data
         """
-        X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8,test_size=0.2,random_state=100)
-        return X_train, X_test, y_train, y_test
+        try:
+            X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8,test_size=0.2,random_state=100)
+            return X_train, X_test, y_train, y_test
+        except:
+            raise NameError('[Process error] There has been an error while processing.')
 
     def remove_empty_values(self, X):
-        x_filtered = []
-        for x in X:
-            if x == x:
-                x_filtered.append(x)
-        return x_filtered
+        try:
+            x_filtered = []
+            for x in X:
+                if x == x:
+                    x_filtered.append(x)
+            return x_filtered
+        except:
+            raise NameError('[Process error] There has been an error while processing.')
 
     def describe(self, features_names, X):
         """
         returns a Feature object list with each
         mean, std, quartiles, min & max values
         """
-        i = 0
-        features = []
-        for x in X:
-            feature = Feature(features_names[i], self.remove_empty_values(x))
-            features.append(feature)
-            i +=1
-        return features
+        try:
+            i = 0
+            features = []
+            for x in X:
+                feature = Feature(features_names[i], self.remove_empty_values(x))
+                features.append(feature)
+                i +=1
+            return features
+        except:
+            raise NameError('[Process error] There has been an error while processing.')
 
     def feature_scale_normalise(self, X):
         """
         Normalises & Standardise each feature vector in X
         using Feature class
         """
-        Xnorm = []
-        features_describe = self.describe(self.features, X)
-        for i in range(len(X)):
-            self.mean.append(features_describe[i].mean)
-            self.stdev.append(features_describe[i].std)
-            Xnorm.append((X[i] - self.mean[i]) / self.stdev[i])
-        Xnorm = np.array(Xnorm)
-        Xnorm = Xnorm.T
-        Xnorm = np.insert(Xnorm, 0, 1, axis=1)
-        return Xnorm
+        try:
+            Xnorm = []
+            features_describe = self.describe(self.features, X)
+            for i in range(len(X)):
+                self.mean.append(features_describe[i].mean)
+                self.stdev.append(features_describe[i].std)
+                Xnorm.append((X[i] - self.mean[i]) / self.stdev[i])
+            Xnorm = np.array(Xnorm)
+            Xnorm = Xnorm.T
+            Xnorm = np.insert(Xnorm, 0, 1, axis=1)
+            return Xnorm
+        except:
+            raise NameError('[Process error] There has been an error while processing.')
 
     def one_hot_encoding(self, y):
         """
@@ -119,29 +134,38 @@ class LogisticRegression(object):
         Hufflepuff = [ 0 0 1 0 ]
         Ravenclaw = [ 0 0 0 1 ]
         """
-        y_encoded = []
-        m = y.shape[0]
-        for i in range(m):
-            house_index = self.houses.index(y[i][0])
-            y_encoded.append([0, 0, 0, 0])
-            y_encoded[i][house_index] = 1
-        y_encoded = np.array(y_encoded)
-        return y_encoded
+        try:
+            y_encoded = []
+            m = y.shape[0]
+            for i in range(m):
+                house_index = self.houses.index(y[i][0])
+                y_encoded.append([0, 0, 0, 0])
+                y_encoded[i][house_index] = 1
+            y_encoded = np.array(y_encoded)
+            return y_encoded
+        except:
+            raise NameError('[Process error] There has been an error while processing.')
 
     def pre_activation(self, X, theta):
         """
         Dot product of weights (θ vector) and features (X)
         will be used in activation (sigmoid)
         """
-        ret = np.dot(X, theta)
-        return ret
+        try:
+            ret = np.dot(X, theta)
+            return ret
+        except:
+            raise NameError('[Process error] There has been an error while processing.')
 
     def activation(self, z):
         """
         The activated function is used in the hypothesis.
         Here we use sigmoid function
         """
-        return 1 / (1 + np.exp(-z))
+        try:
+            return 1 / (1 + np.exp(-z))
+        except:
+            raise NameError('[Process error] There has been an error while processing.')
 
     def hypothesis(self, X, weights):
         """
@@ -152,9 +176,12 @@ class LogisticRegression(object):
             **reutrn: (Numpy vector)**
                 *0 or 1
         """
-        Z = self.pre_activation(X, weights)
-        G = self.activation(Z)
-        return G
+        try:
+            Z = self.pre_activation(X, weights)
+            H = self.activation(Z)
+            return H
+        except:
+            raise NameError('[Process error] There has been an error while processing.')
 
     def cost(self, X, y, theta):
         """
@@ -180,55 +207,65 @@ class LogisticRegression(object):
         """
         Gradient descent algorithm to update theta values
         """
-        m = X.shape[0]
-        cost = []
-        for _ in range(iter):
-            loss = (self.hypothesis(X, self.thetas) - y).T
-            loss_per_feature = np.dot(loss, X).T
-            self.thetas -= alpha * (1/m) * loss_per_feature
-        return self.thetas
-            # cost.append(self.cost(X, y, self.theta))
-        # return self.thetas, cost
+        try:
+            m = X.shape[0]
+            cost = []
+            for _ in range(iter):
+                loss = (self.hypothesis(X, self.thetas) - y).T
+                loss_per_feature = np.dot(loss, X).T
+                self.thetas -= alpha * (1/m) * loss_per_feature
+            return self.thetas
+                # cost.append(self.cost(X, y, self.theta))
+            # return self.thetas, cost
+        except:
+            raise NameError('[Process error] There has been an error while processing.')
 
     def H_from_probability_to_absolute_values(self, X):
-        m = X.shape[0]
-        H_absolute = []
-        H_pobability = self.hypothesis(X, self.thetas)
-        for i in range(m):
-            H_absolute.append([0, 0, 0, 0])
-            H_absolute[i][np.argmax(H_pobability[i])] = 1
-        H_absolute = np.array(H_absolute)
-        return H_absolute
+        try:
+            m = X.shape[0]
+            H_absolute = []
+            H_pobability = self.hypothesis(X, self.thetas)
+            for i in range(m):
+                H_absolute.append([0, 0, 0, 0])
+                H_absolute[i][np.argmax(H_pobability[i])] = 1
+            H_absolute = np.array(H_absolute)
+            return H_absolute
+        except:
+            raise NameError('[Process error] There has been an error while processing.')
 
     def validate(self, result, y):
         """
         Compares X_test predictions with actual result
         """
-        incorrect = 0
-        m = result.shape[0]
-        for i in range(m):
-            pos = np.argmax(y[i])
-            house = self.houses[pos]
-            if result[i] != house:
-                incorrect += 1
-                print('i = {} ||  predicted = {} || actual = {}'.format(i, result[i], house))
-        print("Test results : {}".format((m - incorrect)/m))
+        try:
+            incorrect = 0
+            m = result.shape[0]
+            for i in range(m):
+                pos = np.argmax(y[i])
+                house = self.houses[pos]
+                if result[i] != house:
+                    incorrect += 1
+                    print('i = {} ||  predicted = {} || actual = {}'.format(i, result[i], house))
+            print("Test results : {}".format((m - incorrect)/m))
+        except:
+            raise NameError('[Process error] There has been an error while processing.')
 
     def predict(self, X):
         """
-        Predict house name of student given his grades
+        Predict house name of student given his grades already normalized
         """
-        # X_norm = self.feature_scale_normalise(X)
-        prediction = self.hypothesis(X, self.thetas)
-        result = []
-        index = []
-        for i in range(prediction.shape[0]):
-            house_index = np.argmax(prediction[i])
-            result.append(self.houses[house_index])
-        result = np.array(result)
-        df = pd.DataFrame(data=result, columns=["Hogwarts Houses"])
-        df.index.name = "Index"
-        df.to_csv('houses.csv', index=True)
-        return result
-
+        try:
+            prediction = self.hypothesis(X, self.thetas)
+            result = []
+            index = []
+            for i in range(prediction.shape[0]):
+                house_index = np.argmax(prediction[i])
+                result.append(self.houses[house_index])
+            result = np.array(result)
+            df = pd.DataFrame(data=result, columns=["Hogwarts Houses"])
+            df.index.name = "Index"
+            df.to_csv('houses.csv', index=True)
+            return result
+        except:
+            raise NameError('[Process error] There has been an error while processing.')
     
