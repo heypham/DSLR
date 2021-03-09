070114188df1e1c54f93688b2145e62fc30bfd56
Emilie Pham <epham@student.42.fr>
2021-03-09 15:36:35 +0100

    ADDED Predictor OK

--- logreg_predict.py
+++ logreg_predict.py
@@ -1,1 +1,27 @@
 #/usr/bin/python3
+
+try:
+    from classes.logisticregression import LogisticRegression
+    import pickle
+except NameError as e:
+    print(e)
+    print('[Import error] Please run <pip install -r requirements.txt>')
+    exit()
+
+def main():
+    try:
+        saved_model = open("logreg_model.42", "rb")
+        model = pickle.load(saved_model)
+        saved_model.close()
+
+        args = model.parse_arg()
+        X, y, features = model.read_csv(args.datafile)
+        X_filled = model.fill_data(X, model.mean)
+        X_norm = model.feature_scale_normalise(X_filled)
+        
+        prediction = model.predict(X_norm)
+    except NameError as e:
+        print(e)
+
+if __name__ == '__main__':
+    main()
